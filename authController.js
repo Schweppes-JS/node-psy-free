9115be8ab4f249a700f89dc978999b072594c787
Dmytro Shvets <shvetsdmytro90@ukr.net>
2022-08-13 00:23:45 +0300

    init app

---
+++ authController.js
@@ -0,0 +1,18 @@
+class authController {
+  async registration(req, res) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req, res) {
+    try {
+    } catch (e) {}
+  }
+
+  async getUsers(req, res) {
+    try {
+    } catch (e) {}
+  }
+}
+
+export default new authController();
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import authRouter from './authRouter.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://Schweppes:asd761326@cluster0.1tdrgh7.mongodb.net/psy-free?retryWrites=true&w=majority');
+    app.listen(PORT, () => console.log(`server started on ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
