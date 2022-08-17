ddb5038ab1f4639714aa76bc65f9716ef7c3b44c
Dmytro Shvets <shvetsdmytro90@ukr.net>
2022-08-18 00:19:33 +0300

    implemented sending access token after login

--- authController.js
+++ authController.js
@@ -1,9 +1,13 @@
 import bcrypt from 'bcryptjs';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import Psychologist from './models/Psychologist.cjs';
 import Patient from './models/Patient.cjs';
 import Role from './models/Role.cjs';
+import { secret } from './config.js';
+
+const getAccessToken = (id, role) => jwt.sign({ id, role }, secret, { expiresIn: '24h' });
 
 class authController {
   async registration(req, res) {
@@ -11,8 +15,8 @@ class authController {
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(400).json(errors);
       if (req.body.isPsychologist) {
-        const candidateEmail = await Psychologist.findOne({ email: req.body.email });
-        if (candidateEmail) return res.status(400).json({ message: 'User with this email already registered' });
+        const psychologistEmail = await Psychologist.findOne({ email: req.body.email });
+        if (psychologistEmail) return res.status(400).json({ message: 'User with this email already registered' });
         const psychologistRole = await Role.findOne({ value: 'PSYCHOLOGIST' });
         const psychologist = new Psychologist({
           password: bcrypt.hashSync(req.body.password, 7),
@@ -24,10 +28,10 @@ class authController {
         await psychologist.save();
         return res.status(200).json(psychologist);
       } else {
-        const candidateName = await Patient.findOne({ nickname: req.body.nickname });
-        if (candidateName) return res.status(400).json({ message: 'User with this nickname already registered' });
-        const candidateEmail = await Patient.findOne({ email: req.body.email });
-        if (candidateEmail) return res.status(400).json({ message: 'User with this email already registered' });
+        const patientName = await Patient.findOne({ nickname: req.body.nickname });
+        if (patientName) return res.status(400).json({ message: 'User with this nickname already registered' });
+        const patientEmail = await Patient.findOne({ email: req.body.email });
+        if (patientEmail) return res.status(400).json({ message: 'User with this email already registered' });
         const patientRole = await Role.findOne({ value: 'PATIENT' });
         const patient = new Patient({
           password: bcrypt.hashSync(req.body.password, 7),
@@ -46,6 +50,11 @@ class authController {
 
   async login(req, res) {
     try {
+      Promise.race([Patient.findOne({ email: req.body.email }), Psychologist.findOne({ email: req.body.email })]).then((user) => {
+        if (user && bcrypt.compareSync(req.body.password, user.password)) {
+          return res.json({ token: getAccessToken(user._id, user.roles) });
+        } else return res.status(400).json({ message: 'Email or password is incorect' });
+      });
     } catch (e) {
       res.status(400).json({ message: 'Login error' });
     }
@@ -53,10 +62,9 @@ class authController {
 
   async getUsers(req, res) {
     try {
-      const adminRole = new Role({ value: 'ADMIN' });
-      await adminRole.save();
-      res.json('server work');
-    } catch (e) {}
+    } catch (e) {
+      res.status(400).json({ message: 'Get all user error' });
+    }
   }
 }
 
