599d4be06e2c739998c0b3f5cb561039d45df063
Dmytro Shvets <shvetsdmytro90@ukr.net>
2022-09-20 23:23:04 +0300

    added cors package

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import express from 'express';
 import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
 
 import authRouter from './authRouter.js';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/auth', authRouter);
